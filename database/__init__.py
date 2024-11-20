1849c4639b39c42c0bca5d776c596e1ab0db6eee
Lefteris Kalligiannakis <70850509+LefterisKalligiannakis@users.noreply.github.com>
2024-11-20 15:37:09 +0200

    minor changes 2

--- database/__init__.py
+++ database/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 connection_string = "mssql+pyodbc://ALFS_DB:Ew6uEb8Y@apollo.in.cs.ucy.ac.cy/ALFS_DB?driver=ODBC+Driver+17+for+SQL+Server"
 engine = create_engine(connection_string)
 
+
 if __name__ == '__main__':
     with engine.connect() as connection:
         print("kostis")#with engine.connect as connection:
--- scripts/addNewUser.py
+++ scripts/addNewUser.py
@@ -2,8 +2,12 @@ from database import *
 from sqlalchemy import text
 
 def AddNewUserToDb(Fname,Lname,Email,Password):
-    qry=f"""INSERT INTO [Users] ([Fname],[Lname],[Email],[Password]) Values({Fname},{Lname},{Email},{Password});"""
+    qry = """INSERT INTO Users (Fname, Lname, Email, Password) VALUES (:Fname, :Lname, :Email, :Password);"""
+    
     with engine.connect() as connection:
-        connection.execute(text(qry))
+        connection.execute(
+            text(qry),
+            {"Fname": Fname, "Lname": Lname, "Email": Email, "Password": Password}
+        )
         connection.commit()
-        connection.close()
+        connection.close()   
