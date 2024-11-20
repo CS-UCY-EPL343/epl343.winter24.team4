e68417ca5a79df465b7de39570a2e353057e5f4a
Lefteris Kalligiannakis <70850509+LefterisKalligiannakis@users.noreply.github.com>
2024-11-20 13:24:47 +0200

    db script fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from backend.blueprints import register_blueprints  
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 
 def create_app():
@@ -16,6 +16,13 @@ if __name__ == '__main__':
     connection_string = "mssql+pyodbc://ALFS_DB:Ew6uEb8Y@apollo.in.cs.ucy.ac.cy/ALFS_DB?driver=ODBC+Driver+17+for+SQL+Server"
 
     engine = create_engine(connection_string)  #with engine.connect as connection:
+    
+    #with engine.connect() as connection:
+    #    connection.execute(text(qry))
+    #    connection.commit()
+    #    connection.close()
+    #    print('done')   
+    
     
     app.run(debug=True)  
     
---
+++ backend/database/__init__.py
@@ -0,0 +1,1 @@
+from backend.database.scripts import userQueries
