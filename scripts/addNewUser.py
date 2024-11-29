d916261cd22f910095e6e608a1460170e165c9b3
Pantelis <pantelkan16@gmail.com>
2024-11-29 14:45:59 +0200

    Added the register user query.

--- scripts/addNewUser.py
+++ scripts/addNewUser.py
@@ -1,13 +1,21 @@
 from database import *
 from sqlalchemy import text
+from database import execute_query
 
-def AddNewUserToDb(Fname,Lname,Email,Password):
+def AddNewUserToDb(Fname, Lname, Email, Password):
+    """
+    Adds a new user to the database using the execute_query function.
+
+    Args:
+        Fname (str): First name of the user.
+        Lname (str): Last name of the user.
+        Email (str): Email address of the user.
+        Password (str): Password of the user.
+
+    Returns:
+        bool: True if the user was added successfully, False otherwise.
+    """
     qry = """INSERT INTO Users (Fname, Lname, Email, Password) VALUES (:Fname, :Lname, :Email, :Password);"""
     
-    with engine.connect() as connection:
-        connection.execute(
-            text(qry),
-            {"Fname": Fname, "Lname": Lname, "Email": Email, "Password": Password}
-        )
-        connection.commit()
-        connection.close()   
+    # Use execute_query to handle the insertion
+    return execute_query(qry, Fname=Fname, Lname=Lname, Email=Email, Password=Password)
