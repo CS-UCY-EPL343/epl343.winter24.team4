2036d9e50f0c9bb021100b874079bc36fbabe784
Lefteris Kalligiannakis <70850509+LefterisKalligiannakis@users.noreply.github.com>
2024-11-30 18:57:36 +0200

    route for payments

--- backend/blueprints/__init__.py
+++ backend/blueprints/__init__.py
@@ -1,11 +1,13 @@
 from .main import main
 from .calendar import calendar
-from .auth import auth  
+from .auth import auth
+from .payment import payment  
 
 
 # Register all blueprints to app
 def register_blueprints(app):
     app.register_blueprint(main)
     app.register_blueprint(calendar)
-    app.register_blueprint(auth) 
+    app.register_blueprint(auth)
+    app.register_blueprint(payment)  
     
---
+++ backend/blueprints/payment.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, redirect, request, render_template, jsonify
+
+payment = Blueprint('payment', __name__)
+
+@payment.route('/pay', methods=['GET', 'POST'])
+def makePayment():
+    if request.method == 'GET':
+        return render_template('pay.html')
+
+    elif(request.method == 'POST'):
+        pass; 
