52f8a5aa3717149a102543199eb48d1aa92cb3da
Oratis1948 <giorgosoratis1948@gmail.com>
2024-12-01 15:33:37 +0200

    admin endpoint

--- backend/blueprints/__init__.py
+++ backend/blueprints/__init__.py
@@ -1,7 +1,8 @@
 from .main import main
 from .calendar import calendar
 from .auth import auth
-from .payment import payment  
+from .payment import payment
+from .admin import admin
 
 
 # Register all blueprints to app
@@ -9,5 +10,6 @@ def register_blueprints(app):
     app.register_blueprint(main)
     app.register_blueprint(calendar)
     app.register_blueprint(auth)
-    app.register_blueprint(payment)  
+    app.register_blueprint(payment)
+    app.register_blueprint(admin)
     
