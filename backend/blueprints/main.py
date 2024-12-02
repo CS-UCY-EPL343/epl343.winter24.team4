916fb92aa0a98fc5031394d5e86f30df7ec2b17e
Oratis1948 <giorgosoratis1948@gmail.com>
2024-12-03 01:07:34 +0200

    changesUserData

--- backend/blueprints/main.py
+++ backend/blueprints/main.py
@@ -8,10 +8,5 @@ def home():
                            logged_in = 'user_id' in session
                            )
 
-@main.route('/profile')
-def profile():
-    if 'user_id' in session:
-        return render_template("profile.html")
-    else: redirect(url_for('main.home'))
 
 
